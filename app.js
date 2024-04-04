fae339ef1a10fc705b979e4beb9a7d8590e61382
Louie Jay De La Cruz <lgdelacruz@bayad.com>
2024-04-05 01:19:40 +0800

    debug git

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello world!\ntest");
+  res.end("hello world!\ntest1");
 });
 server.listen(3000);
