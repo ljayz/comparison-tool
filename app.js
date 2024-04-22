f1193c1b336169579f225b106976dec4975f0a85
Louie Jay De La Cruz <lgdelacruz@bayad.com>
2024-04-22 10:48:18 +0800

    added products crud

--- app.js
+++ app.js
@@ -1,16 +1,388 @@
 require("dotenv").config();
 const express = require("express");
 const postgres = require("postgres");
+const { flatten, slice, zip } = require("ramda");
 
 const app = express();
 const sql = postgres(process.env.POSTGRES);
 const port = process.env.PORT;
+const table = process.env.STAGE === "dev" ? "comparison_test" : "comparison";
+const tableConfig =
+  process.env.STAGE === "dev" ? "comparison_config_test" : "comparison_config";
+const log = (...arg) => {
+  if (process.env.STAGE === "dev") {
+    console.log(...arg);
+  }
+};
+const getNeededData = (data) => {
+  const {
+    site = "",
+    name = "",
+    itemid = 0,
+    shopid = 0,
+    brand = "",
+    rating = "",
+    sold = "",
+    price = 0,
+    review = "",
+    stock = "",
+    location = "",
+    image,
+    images,
+  } = data;
+
+  return {
+    site,
+    name,
+    itemid,
+    shopid,
+    brand,
+    rating,
+    sold,
+    price,
+    review,
+    stock,
+    location,
+    image,
+    images,
+  };
+};
+
+app.use(express.json());
 
 app.get("/", async (req, res) => {
-  var lists = await sql`SELECT x.* FROM mobile_app_sample_data x`;
-  // console.log(lists);
+  res.send("It works!");
+});
 
-  res.json(lists);
+//get all products limit 10
+app.get("/products", async (req, res) => {
+  try {
+    const page = req.query?.page || "1";
+    const limit = 10;
+    const offset = isNaN(Number(page)) ? 0 : (Number(page) - 1) * limit;
+    const products = await sql`
+      SELECT *
+      FROM ${sql(table)}
+      ORDER BY sort asc
+      LIMIT ${limit}
+      OFFSET ${offset}
+      `;
+
+    log("products", products);
+    log("page", page);
+    log("offset", offset);
+    log("limit", limit);
+    res.json({ status: "success", data: products });
+  } catch (err) {
+    console.error("Error selecting products", err);
+    res.status(500).json({ status: "error", message: "Unknown error occured" });
+  }
+});
+
+//get itemid by site
+app.get("/itemIds/:site", async (req, res) => {
+  try {
+    const itemIds = (
+      await sql`
+      SELECT itemId
+      FROM ${sql(table)}
+      WHERE site=${req.params.site}`
+    ).map((item) => Number(item.itemid));
+
+    log("itemIds", itemIds);
+
+    res.json({
+      status: "success",
+      message: "Item id successfully selected",
+      data: itemIds,
+    });
+  } catch (err) {
+    console.error("Error selecting itemIds", JSON.stringify(req.params), err);
+    res.status(500).send({ status: "error", message: "Unknown error occured" });
+  }
+});
+
+//get product by ids
+app.get("/productIds/:ids", async (req, res) => {
+  try {
+    const paramIds = req.params?.ids || "";
+    if (!paramIds || paramIds === "null") {
+      return res.json({
+        status: "success",
+        message: "No products found",
+        data: [],
+      });
+    }
+    log("paramIds", paramIds);
+
+    const ids = paramIds.split(",");
+    const products = await sql`
+      SELECT *
+      FROM ${sql(table)}
+      WHERE id IN ${sql(ids)}`;
+    // log("products", products);
+
+    res.json({
+      status: "success",
+      message: "Item id successfully selected",
+      data: products,
+    });
+  } catch (err) {
+    console.error("Error selecting itemIds", JSON.stringify(req.params), err);
+    res.status(500).send({ status: "error", message: "Unknown error occured" });
+  }
+});
+
+//batch create
+app.post("/products/batch", async (req, res) => {
+  const reqBody = req.body;
+  const products = reqBody.map((product) => {
+    return {
+      ...getNeededData(product),
+      createdat: sql`now()`,
+    };
+  });
+  log("products", products);
+
+  let lookupItemIds = [],
+    site;
+  for (const product of products) {
+    site = product.site;
+    lookupItemIds.push(product.itemid);
+  }
+
+  const existingItemIds = (
+    await sql`
+    SELECT itemid
+    FROM ${sql(table)}
+    WHERE site=${site}
+    AND itemid IN ${sql(lookupItemIds)}`
+  ).map((item) => Number(item.itemid));
+
+  const productsToInsert = products.filter(
+    (product) => !existingItemIds.includes(Number(product.itemid)),
+  );
+
+  const productsArr = await sql`
+    INSERT INTO ${sql(table)}
+    ${sql(productsToInsert)}
+    returning itemid
+  `;
+
+  log("productsArr", productsArr);
+
+  res.status(201).json({
+    status: "success",
+    message: "Products successfully created",
+    data: {
+      itemIds: productsArr.map((product) => product.itemid),
+    },
+  });
+});
+
+//create
+app.post("/products", async (req, res) => {
+  const productData = {
+    ...getNeededData(req.body),
+    createdat: sql`now()`,
+  };
+
+  log("productData", productData);
+
+  try {
+    const productArr = await sql`
+      INSERT INTO ${sql(table)}
+      ${sql(productData)}
+      returning *
+    `;
+
+    log("productArr", productArr);
+    const product = productArr.shift();
+
+    res.status(201).json({
+      status: "success",
+      message: "Product successfully created",
+      data: product,
+    });
+  } catch (err) {
+    console.error("Error inserting product", JSON.stringify(req.body), err);
+    res.status(500).send({ status: "error", message: "Unknown error occured" });
+  }
+});
+
+//update
+app.put("/products/:id", async (req, res) => {
+  res.send("debug...");
+});
+
+app.patch("/products/sort", async (req, res) => {
+  try {
+    const [status] = await sql`
+      SELECT *
+      FROM ${sql(tableConfig)}
+      WHERE "key"='sortStatus'`;
+
+    if (!status) {
+      console.error("Unable to see sortStatus config");
+      res
+        .status(500)
+        .json({ status: "error", message: "Unknown error occured" });
+      return;
+    }
+
+    if (status.value === "pending") {
+      console.error("Currently updating products sort");
+      res
+        .status(500)
+        .json({ status: "error", message: "Currently updating products sort" });
+      return;
+    }
+
+    await sql`
+            UPDATE ${sql(tableConfig)}
+            SET value='pending'
+            WHERE "key"='sortStatus'`;
+
+    const lazadaProductIdsArr = await sql`
+      SELECT id
+      FROM ${sql(table)}
+      WHERE site='lazada'
+      AND sort IS NULL
+      ORDER BY id ASC`;
+    // log("lazadaProductIdsArr", lazadaProductIdsArr);
+
+    const shopeeProductIdsArr = await sql`
+      SELECT id
+      FROM ${sql(table)}
+      WHERE site='shopee'
+      AND sort IS NULL
+      ORDER BY id ASC`;
+    // log("shopeeProductIdsArr", shopeeProductIdsArr);
+
+    let lazadaProductIds, shopeeProductIds;
+    if (lazadaProductIdsArr.length > shopeeProductIdsArr.length) {
+      shopeeProductIds = shopeeProductIdsArr;
+      lazadaProductIds = slice(
+        0,
+        shopeeProductIdsArr.length,
+        lazadaProductIdsArr,
+      );
+    } else {
+      lazadaProductIds = lazadaProductIdsArr;
+      shopeeProductIds = slice(
+        0,
+        lazadaProductIdsArr.length,
+        shopeeProductIdsArr,
+      );
+    }
+    log("shopeeProductIds", shopeeProductIds, shopeeProductIds.length);
+    log("lazadaProductIds", lazadaProductIds, lazadaProductIds.length);
+
+    if (shopeeProductIds.length === 0 && lazadaProductIds.length === 0) {
+      await sql`
+              UPDATE ${sql(tableConfig)}
+              SET value='free'
+              WHERE "key"='sortStatus'`;
+
+      res.json({ status: "success", message: "No product updated" });
+      return;
+    }
+
+    const zipProductIds = flatten(zip(shopeeProductIds, lazadaProductIds));
+    log("zipProductIds", zipProductIds);
+
+    for (const product of zipProductIds) {
+      await new Promise((resolve) => setTimeout(resolve, 1));
+      await sql`
+        UPDATE ${sql(table)}
+        SET sort=${sql`now()`}
+        WHERE id=${product.id}`;
+    }
+
+    await sql`
+            UPDATE ${sql(tableConfig)}
+            SET value='free'
+            WHERE "key"='sortStatus'`;
+
+    res.json({ status: "success", message: "Products successfully updated" });
+  } catch (err) {
+    console.error("Error updating products sort", err);
+    res.status(500).json({ status: "error", message: "Unknown error occured" });
+  }
+});
+
+//update specific column
+app.patch("/products/:id", async (req, res) => {
+  res.send("debug...");
+});
+
+//batch delete
+app.delete("/products/batch", async (req, res) => {
+  if (!req.body.itemIds || !req.body.site) {
+    return res
+      .status(404)
+      .send({ status: "error", message: "Products not found" });
+  }
+
+  try {
+    const deletedProducts = await sql`
+      DELETE
+      FROM ${sql(table)}
+      WHERE
+        site=${req.body.site}
+      AND
+        itemid IN ${sql(req.body.itemIds)}
+      returning itemid
+    `;
+
+    log("deletedProducts", deletedProducts);
+
+    res.status(205).send();
+  } catch (err) {
+    console.error("Error deleting product", JSON.stringify(req.params), err);
+    res.status(500).json({ status: "error", message: "Unknown error occured" });
+  }
+});
+
+//delete
+app.delete("/products/:itemId/:site", async (req, res) => {
+  if (!req.params.itemId || !req.params.site) {
+    return res
+      .status(404)
+      .send({ status: "error", message: "Product not found" });
+  }
+
+  try {
+    const productArr = await sql`
+      SELECT
+        id
+      FROM ${sql(table)}
+      WHERE
+        site=${req.params.site}
+      AND
+        itemid=${req.params.itemId}`;
+
+    log("productArr", productArr);
+
+    if (!productArr.length) {
+      return res
+        .status(404)
+        .send({ status: "error", message: "Product not found" });
+    }
+
+    const product = productArr.shift();
+
+    await sql`
+      DELETE
+      FROM ${sql(table)}
+      WHERE id=${product.id}
+      returning itemid`;
+
+    res.status(205).send();
+  } catch (err) {
+    console.error("Error deleting product", JSON.stringify(req.params), err);
+    res.status(500).json({ status: "error", message: "Unknown error occured" });
+  }
 });
 
 app.listen(port, () => {
---
+++ db.sql
@@ -0,0 +1,39 @@
+CREATE TABLE comparison (
+    id bigint NOT NULL,
+    site character varying NOT NULL,
+    name character varying NOT NULL,
+    itemid numeric NOT NULL,
+    shopid numeric NOT NULL,
+    brand character varying,
+    rating numeric,
+    sold character varying,
+    price numeric NOT NULL,
+    review character varying,
+    stock character varying,
+    location character varying,
+    image character varying,
+    images json,
+    sort timestamp without time zone,
+    createdat timestamp without time zone NOT NULL,
+    updatedat timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE SEQUENCE comparison_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER TABLE ONLY comparison ALTER COLUMN id SET DEFAULT nextval('comparison_id_seq'::regclass);
+ALTER TABLE ONLY comparison ADD CONSTRAINT comparison_id_pk PRIMARY KEY (id);
+CREATE INDEX comparison_itemid_idx ON comparison USING btree (itemid);
+CREATE INDEX comparison_itemid_site_idx ON comparison USING btree (itemid, site);
+CREATE INDEX comparison_site_idx ON comparison USING btree (site);
+
+CREATE TABLE comparison_config (
+    key character varying NOT NULL,
+    value text
+);
+ALTER TABLE ONLY comparison_config
+    ADD CONSTRAINT comparison_config_pk PRIMARY KEY (key);
+INSERT INTO comparison_config VALUES ('sortStatus', 'free');
